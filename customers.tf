9cd9975b8217c5a41c4beb27297cbc1102861b1d
Niksa Jadric <43811397+njadric@users.noreply.github.com>
2023-03-02 21:20:23 +0000

    Add files via upload

---
+++ customers.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 1
+  db_name              = "customer_data"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+}
---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "ACCESS_KEY" {}
+variable "SECRET_KEY" {}
+
+variable "REGION" {
+  default = "eu-west-1"
+}
+
+variable "ENVIRONMENT" {
+  default = "TEST"
+}
